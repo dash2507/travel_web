d4ca18e0fb605f39be6f1b21a938797489c2d259
Darshan Kathiriya <darshan@pop-os.localdomain>
2020-03-27 15:01:33 -0300

    Change urls to ELB

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -1,7 +1,9 @@
 // const BOOKING_API = "http://10.0.2.2:5000";
-// const BOOKING_API = "http://54.172.231.118:5000";
-const BOOKING_API = "http://localhost:5001";
-// const PLACES_API = "http://54.172.231.118:5001";
-const PLACES_API = "http://localhost:5002";
+// const BOOKING_API = "http://localhost:5001";
+// const PLACES_API = "http://localhost:5002";
 // const PLACES_API = "https://jsonplaceholder.typicode.com";
+
+const BOOKING_API = "http://travelfirst-env.eba-xn3wxhvc.us-east-1.elasticbeanstalk.com:5001";
+const PLACES_API = "http://travelfirst-env.eba-xn3wxhvc.us-east-1.elasticbeanstalk.com:5002";
+
 export { BOOKING_API, PLACES_API };
